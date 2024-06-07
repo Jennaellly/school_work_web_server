b62fc855860c72ca0dfd28a4333c0f4dcf9810bd
￴￴ <>
2024-06-07 15:03:44 +0700

    up

---
+++ test/school_work_web_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:school_work_web_server/school_work_web_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
